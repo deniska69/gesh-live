4865d12af150c98cce2108063dac4671d7a5b0f2
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-15 11:21:19 +0700

    Обновлена вёрстка страница Фотогалерея

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
 
 const Footer = () => {
-    return (
-        <div className="container-fluid" id="footer">
-            <div className="row align-items-center">
-                <div className="col" id="footer_text">© 2021 ШЕРЕГЕШ LIVE</div>
-            </div>
+  return (
+    <div className="container-fluid" id="footer">
+      <div className="row align-items-center">
+        <div className="col" id="footer_text">
+          © 2021-2022 ШЕРЕГЕШ LIVE
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
